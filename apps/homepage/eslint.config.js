dc5cb46f8b7266686345316f4f13bb5b266499a3
YanniAlshoufi <yanni.school.work@gmail.com>
2025-03-21 16:24:59 +0100

    add backend_and_website project

--- apps/homepage/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-export default [
-  // add more generic rule sets here, such as:
-  ...eslintPluginAstro.configs.recommended,
-  {
-    rules: {},
-  },
-];
