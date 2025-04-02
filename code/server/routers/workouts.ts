47fe417066ced86b6263ef5c3adce1761489a39c
YanniAlshoufi <yanni.school.work@gmail.com>
2025-04-02 17:43:16 +0200

    remove next version and add in a tRPC-only version that I coded in in a tmp project

---
+++ code/server/routers/workouts.ts
@@ -0,0 +1,51 @@
+// import { z } from "zod";
+import { workouts } from "@/data/workoutData";
+
+import { createTRPCRouter, publicProcedure } from "@/trpc";
+
+export const WorkoutsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async () => {
+    console.log(workouts);
+    return workouts;
+  }),
+
+  // setWorkout: publicProcedure
+  //   .input(
+  //     z.object({
+  //       _id: z.string(),
+  //       name: z.string(),
+  //       exercises: z.array(
+  //         z.object({
+  //           _id: z.string(),
+  //           sorting: z.number(),
+  //           numberOfSets: z.number(),
+  //           noteText: z.string(),
+  //         })
+  //       ),
+  //     })
+  //   )
+  //   .mutation(async ({ input: workout }) => {
+  //     const idx = workouts.findIndex((w) => w._id === workout._id);
+  //     const newWorkout = {
+  //       ...workout,
+  //       sorting: workouts[idx]!.sorting,
+  //       exercises: workout.exercises
+  //         .map((e) => ({
+  //           newE: e,
+  //           oldE: workouts[idx]?.exercises.find((ex) => ex._id === e._id),
+  //         }))
+  //         .map(({ newE, oldE }) => ({
+  //           ...newE,
+  //           name: oldE!.name,
+  //           equipmentInfo: oldE!.equipmentInfo,
+  //           involvedMuscles: oldE!.involvedMuscles,
+  //           showcaseImage: oldE!.showcaseImage,
+  //           isUserExercise: oldE!.isUserExercise,
+  //         })),
+  //     };
+
+  //     workouts[idx] = newWorkout;
+
+  //     return newWorkout;
+  // }),
+});
