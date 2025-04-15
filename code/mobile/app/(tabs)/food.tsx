75301ac65be533311914ccc4bae29fc424f84d20
YanniAlshoufi <yanni.school.work@gmail.com>
2025-04-15 20:18:48 +0200

    add rnr

--- code/mobile/app/(tabs)/food.tsx
+++ code/mobile/app/(tabs)/food.tsx
@@ -1,3 +1,4 @@
+import { Progress } from "@/components/ui/progress";
 import { Text } from "@/components/ui/Text";
 import { View } from "react-native";
 
@@ -5,6 +6,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Food is better! :))</Text>
+      <Progress value={60} className="w-9/12" />
     </View>
   );
 }
--- code/mobile/app/_layout.tsx
+++ code/mobile/app/_layout.tsx
@@ -2,14 +2,60 @@ import { StatusBar } from "expo-status-bar";
 import "../global.css";
 import { Stack } from "expo-router";
 import { TRPCReactProvider } from "@/utils/react";
+import {
+  Theme,
+  ThemeProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { NAV_THEME } from "@/lib/constants";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useColorScheme } from "@/lib/userColorScheme";
+import { Platform } from "react-native";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
 
 export default function MainLayout() {
+  const hasMounted = useRef(false);
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background");
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
-    <TRPCReactProvider>
-      <StatusBar style="auto" />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </TRPCReactProvider>
+    <ThemeProvider value={LIGHT_THEME}>
+      <TRPCReactProvider>
+        <StatusBar style={"light"} />
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </TRPCReactProvider>
+    </ThemeProvider>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? useEffect
+    : useLayoutEffect;
