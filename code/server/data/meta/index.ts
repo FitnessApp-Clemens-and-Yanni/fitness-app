1f9af1381f13dcb10890ba117d611144ce7e95cf
YanniAlshoufi <yanni.school.work@gmail.com>
2025-05-09 16:43:20 +0200

    try to make docker use correct mongo settings

--- code/server/data/meta/index.ts
+++ code/server/data/meta/index.ts
@@ -2,39 +2,49 @@ import { MongoClient } from "mongodb";
 import {
   ExerciseSnapshot,
   FINISHED_WORKOUTS_COLLECTION,
-  FinishedWorkout, NUTRITIONAL_VALUE_OF_DAY_COLLECTION, NutritionalValueOfDay,
-  SNAPSHOTS_COLLECTION, TARGET_NUTRITIONAL_VALUE_COLLECTION, TargetNutritionalValue,
+  FinishedWorkout,
+  NUTRITIONAL_VALUE_OF_DAY_COLLECTION,
+  NutritionalValueOfDay,
+  SNAPSHOTS_COLLECTION,
+  TARGET_NUTRITIONAL_VALUE_COLLECTION,
+  TargetNutritionalValue,
   Workout,
   WORKOUTS_COLLECTION,
 } from "./models";
 import { workouts } from "../defaults/workoutData";
 import { snapshots } from "../defaults/snapshotsData";
 import { configDotenv } from "dotenv";
-import {targetNutritionalValue} from "@/data/defaults/targetNutritionalValueData";
+import { targetNutritionalValue } from "@/data/defaults/targetNutritionalValueData";
 
 configDotenv({ path: ".env" });
-const client = new MongoClient(process.env.DB_CONN_STRING!);
+const client = new MongoClient(
+  (process.env.IS_DEV ?? "no") === "yes"
+    ? "mongodb://localhost:27017"
+    : "mongodb://database-fitness-app-mongodb:27017",
+);
 
 export const db = client.db();
 ensureCollectionsInitializedAndPopulated();
 
 async function ensureCollectionsInitializedAndPopulated() {
   const workoutsCollection = await db.createCollection<Workout>(
-    WORKOUTS_COLLECTION
+    WORKOUTS_COLLECTION,
   );
   const snapshotsCollection = await db.createCollection<ExerciseSnapshot>(
-    SNAPSHOTS_COLLECTION
+    SNAPSHOTS_COLLECTION,
   );
 
   await db.createCollection<FinishedWorkout>(FINISHED_WORKOUTS_COLLECTION);
 
-  const targetNutritionalValueCollection = await db.createCollection<TargetNutritionalValue>(
-      TARGET_NUTRITIONAL_VALUE_COLLECTION
-  );
+  const targetNutritionalValueCollection =
+    await db.createCollection<TargetNutritionalValue>(
+      TARGET_NUTRITIONAL_VALUE_COLLECTION,
+    );
 
-  const nutritionalValueOfDayCollection = await db.createCollection<NutritionalValueOfDay>(
-    NUTRITIONAL_VALUE_OF_DAY_COLLECTION
-  );
+  const nutritionalValueOfDayCollection =
+    await db.createCollection<NutritionalValueOfDay>(
+      NUTRITIONAL_VALUE_OF_DAY_COLLECTION,
+    );
 
   if ((await workoutsCollection.countDocuments()) === 0) {
     workoutsCollection.insertMany(workouts);
@@ -44,7 +54,7 @@ async function ensureCollectionsInitializedAndPopulated() {
     snapshotsCollection.insertMany(snapshots);
   }
 
-  if(await targetNutritionalValueCollection.countDocuments() === 0) {
+  if ((await targetNutritionalValueCollection.countDocuments()) === 0) {
     targetNutritionalValueCollection.insertMany(targetNutritionalValue);
   }
 }
